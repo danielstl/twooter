885de720835e790ba3c079a14d6269d78d9f93f5
danielstl <danielstl@github.io>
2019-05-19 16:54:34 +0100

    additional documentation for MessageCell and fixes to prevent content appearing for empty cells

--- src/danstl/twooter/gui/MessageCell.java
+++ src/danstl/twooter/gui/MessageCell.java
@@ -4,18 +4,24 @@ import danstl.twooter.JsonTwoot;
 import javafx.scene.control.ListCell;
 import twooter.Message;
 
+/**
+ * Message cell for formatting messages as they appear in the list on the main page feed
+ */
 public class MessageCell extends ListCell<Message> {
 
+    /**
+     * Method for displaying the message
+     */
     @Override
     protected void updateItem(Message item, boolean empty) {
         super.updateItem(item, empty);
 
-        if (item == null) {
-            setText("Empty message");
+        if (item == null || empty) { //clear the content if message is empty
+            setText(null);
             return;
         }
 
-        JsonTwoot json = JsonTwoot.resolve(item.message);
+        JsonTwoot json = JsonTwoot.resolve(item.message); //attempt to identify if this is a json twoot. Will be formatted differently if so
 
         if (json == null) { //a normal twoot
             setText(item.name + "\n" + item.message);
